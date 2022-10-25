cd168b31268819643b220db06e34124829489dbd
Ben Johnson <bkj.322@gmail.com>
2022-10-25 09:50:29 -0400

    Update rcode.py

--- rcode/rcode.py
+++ rcode/rcode.py
@@ -3,11 +3,13 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from subprocess import check_output
 
+DEFAULT_PATH = "./tmp.png"
+
 def rcode(path):
     """ str path: path to open """
     _ = check_output('code %s' % path, shell=True)
 
-def show_plot(outpath='./tmp.png', *args, **kwargs):
+def show_plot(outpath=DEFAULT_PATH, *args, **kwargs):
     """ str outpath: path to write figure """
     plt.savefig(outpath, *args, **kwargs)
     plt.close()
